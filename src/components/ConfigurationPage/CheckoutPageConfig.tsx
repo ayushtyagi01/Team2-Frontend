9d2630bfdf419a3399721b60f63945b068f63ab4
Saurabh-crypto16 <prakashsaurabh161228@gmail.com>
2023-04-26 10:30:11 +0530

    Added config file pre populated

--- src/components/ConfigurationPage/CheckoutPageConfig.tsx
+++ src/components/ConfigurationPage/CheckoutPageConfig.tsx
@@ -1,6 +1,6 @@
 import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 import axios from "axios";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { jwtToken } from "../../redux/slice/UserSlice";
 import { checkoutPageConfigUtil } from "../../util/configurationUtil/CheckoutPageConfigUtil";
@@ -26,8 +26,11 @@ const BillingInfo = [
   "Phone",
   "Email",
 ];
-const PaymentInfo = ["Card Name", "Expiry Month", "Expiry Year", "CVV"];
+const PaymentInfo = ["Card Name", "Exp MM", "Exp YY", "CVV"];
+
 const CheckoutPageConfig: React.FC = () => {
+  const [checkoutConfig, setCheckoutConfig] = useState<any>(null);
+
   const handleClickTraveller = (e: React.MouseEvent<HTMLLabelElement>) => {
     const checkbox = e.target as HTMLInputElement;
     const checkboxValue = checkbox.value;
@@ -60,65 +63,144 @@ const CheckoutPageConfig: React.FC = () => {
   };
   const token = useAppSelector(jwtToken);
 
-  const updateConfig = async (filteredCheckoutPageConfigUtil:any)=>{
-    await axios.post(process.env.REACT_APP_UPDATE_CONFIG!, {
-        token:token,
+  const updateConfig = async (filteredCheckoutPageConfigUtil: any) => {
+    await axios
+      .post(process.env.REACT_APP_UPDATE_CONFIG!, {
+        token: token,
         fileName: "CheckoutConfig.txt",
-        folderName:"hotel-1/",
-        fileContent: filteredCheckoutPageConfigUtil
-    }).then(response=>response.data)
-    .catch(error=>console.log("error"));
-  }
+        folderName: "hotel-1/",
+        fileContent: filteredCheckoutPageConfigUtil,
+      })
+      .then((response) => response.data)
+      .catch((error) => console.log("error"));
+    console.log("value", filteredCheckoutPageConfigUtil);
+  };
 
   const handleConfig = () => {
     const filteredCheckoutPageConfigUtil = checkoutPageConfigUtil.INFO.map(
       (page) => {
-        const filteredInputs = page.inputs.map((inputsArray) =>
-          inputsArray
+        const filteredInputs = page.inputs.map((inputsArray) => {
+          return inputsArray
             .filter((input) => input.present)
-            .map(({ present, ...rest }) => rest)
-        );
+            .map(({ present, ...rest }) => rest);
+        });
         return { ...page, inputs: filteredInputs };
       }
     );
+
     updateConfig(filteredCheckoutPageConfigUtil);
   };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://team2-configuration.s3.ap-south-1.amazonaws.com/hotel-1/CheckoutConfig.txt"
+      )
+      .then((config) => {
+        const result = config.data.INFO.map((obj: any) => {
+          const flattenedInputs = obj.inputs.reduce(
+            (acc: any, curr: any) => acc.concat(curr),
+            []
+          );
+          return flattenedInputs.map((input: any) => ({
+            label: input.label,
+            type: input.type,
+          }));
+        });
+
+        result[0].map((res: any) => {
+          checkoutPageConfigUtil.INFO[0].inputs.forEach((label) =>
+            label.forEach(
+              (type) => type.label === res.label && (type.present = true)
+            )
+          );
+        });
+        result[1].map((res: any) => {
+          checkoutPageConfigUtil.INFO[1].inputs.forEach((label) =>
+            label.forEach(
+              (type) => type.label === res.label && (type.present = true)
+            )
+          );
+        });
+        result[2].map((res: any) => {
+          checkoutPageConfigUtil.INFO[2].inputs.forEach((label) =>
+            label.forEach(
+              (type) => type.label === res.label && (type.present = true)
+            )
+          );
+        });
+
+        setCheckoutConfig(result);
+      });
+  }, []);
+
   return (
     <>
       <div className="heading-config">Checkout Page Configuration</div>
       <div className="info-label">Traveller Info</div>
-      {TravellerInfo.map((type: any) => (
-        <FormControlLabel
-          value="End"
-          control={<Checkbox value={type}/>}
-          label={type}
-          labelPlacement="end"
-          onClick={(e) => handleClickTraveller(e)}
-          className="info-check"
-        />
-      ))}
+      {checkoutConfig &&
+        checkoutConfig[0] &&
+        TravellerInfo.map((type: any) => (
+          <FormControlLabel
+            value="End"
+            control={
+              <Checkbox
+                value={type}
+                defaultChecked={
+                  checkoutConfig[0].filter((val: any) => val.label == type)
+                    .length > 0
+                }
+              />
+            }
+            label={type}
+            labelPlacement="end"
+            onClick={(e) => handleClickTraveller(e)}
+            className="info-check"
+          />
+        ))}
+
       <div className="info-label">Billing Info</div>
-      {BillingInfo.map((type: any) => (
-        <FormControlLabel
-          value="End"
-          control={<Checkbox value={type}/>}
-          label={type}
-          labelPlacement="end"
-          onClick={(e) => handleClickBilling(e)}
-          className="info-check"
-        />
-      ))}
+      {checkoutConfig &&
+        checkoutConfig[1] &&
+        BillingInfo.map((type: any) => (
+          <FormControlLabel
+            value="End"
+            control={
+              <Checkbox
+                value={type}
+                defaultChecked={
+                  checkoutConfig[1].filter((val: any) => val.label == type)
+                    .length > 0
+                }
+              />
+            }
+            label={type}
+            labelPlacement="end"
+            onClick={(e) => handleClickBilling(e)}
+            className="info-check"
+          />
+        ))}
       <div className="info-label">Payment Info</div>
-      {PaymentInfo.map((type: any) => (
-        <FormControlLabel
-          value="End"
-          control={<Checkbox value={type}/>}
-          label={type}
-          labelPlacement="end"
-          onClick={(e) => handleClickPayment(e)}
-          className="info-check"
-        />
-      ))}
+      {checkoutConfig &&
+        checkoutConfig[2] &&
+        PaymentInfo.map((type: any) => (
+          <FormControlLabel
+            value="End"
+            control={
+              <Checkbox
+                value={type}
+                defaultChecked={
+                  checkoutConfig[2].filter((val: any) => val.label == type)
+                    .length > 0
+                }
+              />
+            }
+            label={type}
+            labelPlacement="end"
+            onClick={(e) => handleClickPayment(e)}
+            className="info-check"
+          />
+        ))}
       <Button
         variant="contained"
         onClick={() => handleConfig()}
--- src/components/ConfigurationPage/RoomResultsConfig.tsx
+++ src/components/ConfigurationPage/RoomResultsConfig.tsx
@@ -1,6 +1,6 @@
 import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 import axios from "axios";
-import { createRef, useRef } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { jwtToken } from "../../redux/slice/UserSlice";
 import { LandingPageConfigUtil } from "../../util/configurationUtil/LandingPageConfigUtil";
@@ -20,8 +20,8 @@ const sortType = [
   "Queen Bed Count High",
   "Total Bed Count Low",
   "Total Bed Count High",
-  "Name Asc",
-  "Name Desc",
+  "Name ASC",
+  "Name DESC",
 ];
 const filters = ["Room Type", "Bed Type", "Price Range"];
 const roomType = [
@@ -38,42 +38,71 @@ const RoomResultConfig: React.FC = () => {
   const roomImageRef = useRef<HTMLInputElement[]>([]);
   const roomDescriptionRef = useRef<HTMLInputElement[]>([]);
   const roomAmmenitiesRef = useRef<HTMLInputElement[]>([]);
+  const sortTypeRef = useRef<HTMLInputElement[]>([]);
+  const filterTypeRef = useRef<HTMLInputElement[]>([]);
+
+  const [currentConfig, setCurrentConfig] = useState<any>(null);
 
   const token = useAppSelector(jwtToken);
-
 
   const handleClickSort = (e: React.MouseEvent<HTMLLabelElement>) => {
     const checkbox = e.target as HTMLInputElement;
     const checkboxValue = checkbox.value;
 
-    for (let i = 0; i < roomResultConfig.sortType.length; i++) {
-      if (roomResultConfig.sortType[i].value === checkboxValue) {
-        roomResultConfig.sortType[i].present = true;
-        break;
-      }
-    }
+    roomResultConfig.sortType.forEach(
+      (label) =>
+        label.value === checkboxValue && (label.present = checkbox.checked)
+    );
   };
 
   const handleClickFilter = (e: React.MouseEvent<HTMLLabelElement>) => {
     const checkbox = e.target as HTMLInputElement;
     const checkboxValue = checkbox.value;
+
     roomResultConfig.filterTypes.forEach(
       (label) =>
         label.value === checkboxValue && (label.present = checkbox.checked)
     );
   };
 
-  const updateConfig = async (newRoomResultConfig:any)=>{
-    await axios.post(process.env.REACT_APP_UPDATE_CONFIG!, {
-        token:token,
+  const updateConfig = async (newRoomResultConfig: any) => {
+    console.log("token", token);
+    await axios
+      .post(process.env.REACT_APP_UPDATE_CONFIG!, {
+        token: token,
         fileName: "SearchResultsPage.txt",
-        folderName:"hotel-1/RoomResultsPage/",
-        fileContent: newRoomResultConfig
-    }).then(response=>response.data)
-    .catch(error=>console.log("error"));
-  }
+        folderName: "hotel-1/RoomResultsPage/",
+        fileContent: JSON.stringify(newRoomResultConfig),
+      })
+      .then((response) => response.data)
+      .catch((error) => console.log("error"));
+  };
 
   const handleConfig = () => {
+    const checkedLabels: any[] = [];
+    sortTypeRef.current.map((input) => {
+      if (input.checked) {
+        checkedLabels.push(input.value.trim());
+      }
+    });
+    checkedLabels.map((checkboxValue: any) => {
+      roomResultConfig.sortType.forEach(
+        (label) => label.value == checkboxValue && (label.present = true)
+      );
+    });
+
+    const checkedLabelsFilter: any[] = [];
+    filterTypeRef.current.map((input) => {
+      if (input.checked) {
+        checkedLabelsFilter.push(input.value);
+      }
+    });
+    checkedLabelsFilter.map((checkboxValue: any) => {
+      roomResultConfig.filterTypes.forEach(
+        (label) => label.value == checkboxValue && (label.present = true)
+      );
+    });
+
     const newRoomResultConfig: any = {
       sortType: roomResultConfig.sortType
         .filter((item) => item.present === true)
@@ -122,63 +151,123 @@ const RoomResultConfig: React.FC = () => {
         }
       ),
     };
+
     updateConfig(newRoomResultConfig);
   };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://team2-configuration.s3.ap-south-1.amazonaws.com/hotel-1/RoomResultsPage/SearchResultsPage.txt"
+      )
+      .then((data: any) => {
+        console.log("data", data.data);
+        setCurrentConfig(data.data);
+      });
+  }, []);
+
   return (
     <>
       <div className="heading-config">Room Results Configuration</div>
       <div className="sort-label">Sort Type</div>
-      {sortType.map((type: any, index: number) => (
-        <>
+      {currentConfig &&
+        sortType.map((type: any, index: number) => (
+          <>
+            <FormControlLabel
+              value="End"
+              control={
+                <Checkbox
+                  value={type}
+                  defaultChecked={
+                    currentConfig.sortType.filter(
+                      (val: any) => val.value == type
+                    ).length > 0
+                  }
+                  inputRef={(ref) => (sortTypeRef.current[index] = ref!)}
+                />
+              }
+              label={type}
+              labelPlacement="end"
+              onClick={(e) => handleClickSort(e)}
+              className="sort-check"
+            />
+            {index % 2 !== 0 && <div></div>}
+          </>
+        ))}
+      <div className="sort-label">Filter Type</div>
+      {currentConfig &&
+        filters.map((filter: any, index: number) => (
           <FormControlLabel
             value="End"
-            control={<Checkbox value={type} />}
-            label={type}
+            control={
+              <Checkbox
+                value={filter}
+                defaultChecked={
+                  currentConfig.filterTypes.filter(
+                    (val: any) => val.value == filter
+                  ).length > 0
+                }
+                inputRef={(ref) => (filterTypeRef.current[index] = ref!)}
+              />
+            }
+            label={filter}
             labelPlacement="end"
-            onClick={(e) => handleClickSort(e)}
-            className="sort-check"
+            onClick={(e) => handleClickFilter(e)}
+            className="filter-check"
           />
-          {index % 2 !== 0 && <div></div>}
-        </>
-      ))}
-      <div className="sort-label">Filter Type</div>
-      {filters.map((filter: any) => (
-        <FormControlLabel
-          value="End"
-          control={<Checkbox value={filter} />}
-          label={filter}
-          labelPlacement="end"
-          onClick={(e) => handleClickFilter(e)}
-          className="filter-check"
-        />
-      ))}
+        ))}
 
       <div className="text-label">
         <div className="label">Enter Resort Tax</div>{" "}
-        <TextField
-          className="textfield-label"
-          inputRef={resortTaxRef}
-          type="number"
-        />
+        {currentConfig && (
+          <TextField
+            className="textfield-label"
+            inputRef={resortTaxRef}
+            type="number"
+            defaultValue={
+              currentConfig.taxes
+                ? currentConfig.taxes[0].factor
+                : roomResultConfig.taxes[0].factor
+            }
+          />
+        )}
       </div>
       <div className="text-label">
         <div className="label">Enter Occupancy Tax</div>{" "}
-        <TextField
-          className="textfield-label"
-          inputRef={occupancyTaxRef}
-          type="number"
-        />
+        {currentConfig && (
+          <TextField
+            className="textfield-label"
+            inputRef={occupancyTaxRef}
+            type="number"
+            defaultValue={
+              currentConfig.taxes
+                ? currentConfig.taxes[1].factor
+                : roomResultConfig.taxes[1].factor
+            }
+          />
+        )}
       </div>
       {roomType.map((type, index: number) => (
         <>
           <div className="room-type">{type}</div>
           <div className="text-label">
-            <div className="label images-label">Enter Image Url(, seprated)</div>{" "}
+            <div className="label images-label">
+              Enter Image Url(, seprated)
+            </div>{" "}
             <TextField
               className="textfield-label"
               inputRef={(input) => {
                 roomImageRef.current[index] = input;
               }}
+              defaultValue={
+                currentConfig && currentConfig.roomImages
+                  ? currentConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].images
+                  : roomResultConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].images
+              }
             />
             <div className="label images-label">Enter Description</div>{" "}
             <TextField
@@ -186,13 +275,33 @@ const RoomResultConfig: React.FC = () => {
               inputRef={(input) => {
                 roomDescriptionRef.current[index] = input;
               }}
+              defaultValue={
+                currentConfig && currentConfig.roomImages
+                  ? currentConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].description
+                  : roomResultConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].description
+              }
             />
-            <div className="label images-label">Enter Ammenities(, seprated)</div>{" "}
+            <div className="label images-label">
+              Enter Ammenities(, seprated)
+            </div>{" "}
             <TextField
               className="textfield-label"
               inputRef={(input) => {
                 roomAmmenitiesRef.current[index] = input;
               }}
+              defaultValue={
+                currentConfig && currentConfig.roomImages
+                  ? currentConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].amnetieis
+                  : roomResultConfig.roomImages.filter(
+                      (val: any) => val.customeName == type
+                    )[0].amnetieis
+              }
             />
           </div>
         </>
